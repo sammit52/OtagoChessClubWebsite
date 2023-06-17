2425d05bf239a60e9d0300a8d2eb22540fd84739
sammit52 <sammit52@kingshigh.school.nz>
2023-06-18 00:02:00 +1200

    Added Template, Main Pages and Static Files

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import re
 from datetime import datetime
+from flask import render_template
 
 from flask import Flask
 
@@ -9,19 +10,16 @@ app = Flask(__name__)
 def home():
     return "Hello, Flask!"
 
+@app.route("/hello/")
 @app.route("/hello/<name>")
-def hello_there(name):
-    now = datetime.now()
-    formatted_now = now.strftime("A%, %d %B, %Y at %X")
+def hello_there(name = None):
+    print(name)
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
     
-    #Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitary text, so we restrict to safe characters only
-    match_object = re.match("[a-zA-Z]+", name)
-
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return content
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
