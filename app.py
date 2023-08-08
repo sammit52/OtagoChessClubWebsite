914cece2e746979f2a7504d8081de0f5c170f785
sammit52 <sammit52@kingshigh.school.nz>
2023-08-08 21:05:57 +1200

    Made Calendar Widget and .json Library Books File

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
 import re
 from datetime import datetime
 from flask import Flask, render_template, url_for, flash, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-
-
+from googleapiclient.discovery import build
 
 app = Flask(__name__)
+
+# Replace with your API key and calendar ID
+API_KEY = 'AIzaSyBrSepztiiinJcyMsWfUjhjcLK9qA8Cm14'
+CALENDAR_ID = 'f87833500104928c8fe435ceb2db6f55e81db34f8af20624b565005f8f51eafe@group.calendar.google.com'
+
+# Build the Google Calendar API service using the API key
+service = build('calendar', 'v3', developerKey=API_KEY)
+
+
+def get_upcoming_events(max_results=4):
+    events_result = service.events().list(calendarId=CALENDAR_ID, maxResults=max_results,
+                                          singleEvents=True, orderBy='startTime').execute()
+    events = events_result.get('items', [])
+    return events
+
+def format_date(date_str):
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    date_parts = date_str.split('-')
+    month = months[int(date_parts[1]) - 1]
+    day = date_parts[2].lstrip('0')  # Remove leading zero from the day
+    formatted_date = f"{month} {day}"
+    return formatted_date
+
+@app.route("/")
+def home():
+    upcoming_events = get_upcoming_events(max_results=5)
+    event_data = []
+    for event in upcoming_events:
+        event_name = event['summary']
+        event_start = event['start'].get('dateTime', event['start'].get('date'))
+        formatted_date = format_date(event_start.split("T")[0])  # Format the date
+        event_data.append((event_name, formatted_date))
+
+    return render_template("home.html", events=event_data)
 
 
 @app.route("/hello/")
@@ -22,9 +54,6 @@ def hello_there(name = None):
 def get_data():
     return app.send_static_file("data.json")
 
-@app.route("/")
-def home():
-    return render_template("home.html")
 
 @app.route("/about")
 def about():
@@ -40,6 +69,7 @@ def notices():
 
 @app.route("/calendar")
 def calendar():
+
     return render_template("calendar.html")
 
 @app.route("/tournaments")
@@ -60,3 +90,5 @@ def ratings():
 def results():
     return render_template("results.html")
 
+if __name__ == "__main__":
+    app.run(debug=True)
